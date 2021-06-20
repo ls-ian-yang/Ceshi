9f7fe3db9a11cc0702b6e913f04ab0593e6001ec
IsaacYaner <47761372+IsaacYaner@users.noreply.github.com>
2021-06-20 12:09:54 +1000

    fetch to linux

--- shit_command.cpp
+++ shit_command.cpp
@@ -9,22 +9,29 @@ int main()
     struct stat buf;
     int result;
  
-    //获得文件状态信息
- 
-    result =stat( "./ceshi.cpp", &buf );
- 
-    //显示文件状态信息
- 
-    if( result != 0 )
-       perror( "Error: " );//并提示出错的原因，如No such file or directory（无此文件或索引）
- 
+    //假设当前文件夹只有一种语言的test文件    
+    result = stat("./test.cpp", &buf);
+    if (result != 0)
+    { 
+        result =stat("./test.c", &buf);
+        if (result != 0)
+        {
+            result =stat("./test.py", &buf);
+            if (result != 0)
+            { 
+                result =stat("./test.cu", &buf);
+            }
+        }
+    }
+    if (result != 0)
+    {
+        perror("Error");
+    }
     else
     {
- 
         printf("Size: %d\n", buf.st_size);
         printf("Creat time: %s", ctime(&buf.st_ctime));
         printf("Access time: %s", ctime(&buf.st_atime));
         printf("Modify time: %s", ctime(&buf.st_mtime));
- 
     }
 }
